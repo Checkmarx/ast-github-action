55ec8c66fe8c1053d99db995f751eef8511901da
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-03-30 12:37:16 -0400

    Initial commit to add config files

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ast-cli-test:latest --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project_type ${PROJECT_TYPE} --project_name ${PROJECT_NAME} -s ${SOURCE}
+ast-cli-test:latest --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project_type ${PROJECT_TYPE} --project_name ${PROJECT_NAME} -s ${ARCHIVE_SOURCE} -d ${PROJECT_SOURCE}
