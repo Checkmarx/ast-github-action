bdcd40d18c27eff2b8806757038f961ab3b5b20a
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-03-30 15:53:58 -0400

    Changed configuration in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run jprannoyn474/ast-cli-test:latest -it --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project_type ${PROJECT_TYPE} --project_name ${PROJECT_NAME} -s ${ARCHIVE_SOURCE} -d ${PROJECT_SOURCE}
+docker exec -it jprannoyn474/ast-cli-test:latest --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project_type ${PROJECT_TYPE} --project_name ${PROJECT_NAME} -s ${ARCHIVE_SOURCE} -d ${PROJECT_SOURCE}
