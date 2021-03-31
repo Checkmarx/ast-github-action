7e770ebcb9b8aff00845ca6a332d8197afbfe716
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-03-31 12:11:23 -0400

    Changed configuration in entrypoint.sh and action.yml to add filters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/app/bin/cx --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project-type ${PROJECT_TYPE} --project-name ${PROJECT_NAME} -d ${PROJECT_SOURCE}
+/app/bin/cx --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project-type ${PROJECT_TYPE} --project-name ${PROJECT_NAME} -d ${PROJECT_SOURCE} --filter ${FILTER}
