89be0d9951b7c7243774df5f670a64ba7970011c
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-04-01 15:54:47 -0400

    Changed configuration in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,14 @@
 #/app/bin/cx --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project-type ${PROJECT_TYPE} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY} --filter ${FILTER}
 /app/bin/cx scan create  -v --base-uri ${BASE_URI} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY}
 if [[ -v ${FILTER} ]]; then
-  --filter ${FILTER}
+  echo "--filter ${FILTER}"
 fi
 if [[ -v ${CLIENT_ID} ]]; then
-  --client-id ${CLIENT_ID}
+  echo "--client-id ${CLIENT_ID}"
 fi
 if [[ -v ${SECRET} ]]; then
-  "--secret" ${SECRET}
+  echo "--secret ${SECRET}"
 fi
 if [[ -v ${PROJECT_TYPE} ]]; then
-  "--project-type" ${PROJECT_TYPE}
+  echo "--project-type ${PROJECT_TYPE}"
 fi
