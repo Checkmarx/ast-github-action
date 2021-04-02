3eec1a05b1a46eda7e0e06875b79593ddc2c5152
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-04-02 03:08:13 -0400

    Changed configuration in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,25 @@
 #!/bin/sh
+args=()
+(( -n $FILTER )) && args+=('--filter') && args+=(${FILTER})
+(( -n $CLIENT_ID )) && args+=('--client-id') && args+=(${CLIENT_ID})
+(( -n $SECRET )) && args+=('--secret') && args+=(${SECRET})
+(( -n $PROJECT_TYPE )) && args+=('--project-type') && args+=(${PROJECT_TYPE})
+
+
+
+/app/bin/cx scan create  -v --base-uri ${BASE_URI} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY} "${args[@]}"
+#/app/bin/cx scan create  -v --base-uri ${BASE_URI} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY} $( ((-v ${FILTER})) && printf %s '--filter ${FILTER}' ) $( ((-v ${CLIENT_ID})) && printf %s '--client-id ${CLIENT_ID}' ) $( ((-v ${SECRET})) && printf %s '--secret ${SECRET}' ) $( ((-v ${PROJECT_TYPE})) && printf %s '--project-type ${PROJECT_TYPE}' )
 #/app/bin/cx --client-id ${CLIENT_ID} --secret ${SECRET} --base-uri ${BASE_URI} --format json scan create --project-type ${PROJECT_TYPE} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY} --filter ${FILTER}
-/app/bin/cx scan create  -v --base-uri ${BASE_URI} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY}
-if [[ -v ${FILTER} ]]; then
-  echo "--filter ${FILTER}"
-fi
-if [[ -v ${CLIENT_ID} ]]; then
-  echo "--client-id ${CLIENT_ID}"
-fi
-if [[ -v ${SECRET} ]]; then
-  echo "--secret ${SECRET}"
-fi
-if [[ -v ${PROJECT_TYPE} ]]; then
-  echo "--project-type ${PROJECT_TYPE}"
-fi
+#/app/bin/cx scan create  -v --base-uri ${BASE_URI} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY}
+#`if [ -v ${FILTER} ]; then
+#  --filter ${FILTER}
+#fi`
+#`if [ -v ${CLIENT_ID} ]]; then
+#  --client-id ${CLIENT_ID}
+#fi`
+#`if [[ -v ${SECRET} ]]; then
+#  --secret ${SECRET}
+#fi`
+#`if [[ -v ${PROJECT_TYPE} ]]; then
+#  --project-type "${PROJECT_TYPE}"
+#fi`
