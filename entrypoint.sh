3897f95a669fdfdc214ff2b15a34d6939f0d0b12
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-04-06 15:34:12 -0400

    Release version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/app/bin/cx scan create --project-type ${PROJECT_TYPE} --project-name ${PROJECT_NAME} -d ${SOURCE_DIRECTORY} --filter ${FILTER} --format json
+/app/bin/cx scan create --project-type ${PROJECT_TYPE} --project-name ${PROJECT_NAME} -d "." --filter ${FILTER} --format json
