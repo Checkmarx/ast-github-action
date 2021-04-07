677ffd702afe8d23cd6300d69dff71cc089027ee
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-04-07 15:07:05 -0400

    removed the print environment command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,2 @@
 #!/bin/sh
-printenv
 /app/bin/cx scan create -v --project-type "${PROJECT_TYPE}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action"
