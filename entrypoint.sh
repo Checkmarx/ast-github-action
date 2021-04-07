266abd6a6bdafeb308507250da0d94393e9537e1
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-04-07 14:44:02 -0400

    Changes to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,2 @@
 #!/bin/sh
-printenv
 /app/bin/cx scan create -v --project-type ${PROJECT_TYPE} --project-name ${PROJECT_NAME} -d "." --filter ${FILTER} --preset-name ${PRESET_NAME} --format json
-/app/bin/cx version
-ls -la /github/workspace/
-cp /github/workspace/*.zip ./cx.zip
-chmod 777 ./cx.zip
-pwd
-find .
