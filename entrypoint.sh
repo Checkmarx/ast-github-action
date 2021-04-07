9eb0bc191092b246dcf09a46ca61c28c44eaa485
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-04-07 15:00:39 -0400

    Changes to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 printenv
-/app/bin/cx scan create -v --project-type "${PROJECT_TYPE}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json
+/app/bin/cx scan create -v --project-type "${PROJECT_TYPE}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action"
