f74b31e04fd4e5ed848acbe73e9ab799279f0c47
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-04-22 12:21:20 -0400

    Changed the sample file for workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/app/bin/cx scan create -v --project-type "${PROJECT_TYPE}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json ${ADDITIONAL_PARAMS}
+/app/bin/cx scan create -v --project-type "${PROJECT_TYPE}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS}
