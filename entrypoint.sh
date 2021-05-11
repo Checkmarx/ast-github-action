59ffb03886f01b15e667240a39ddd5f792356957
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-05-11 11:56:41 +0100

    fix multiline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-cxscan=`/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS}`
-echo $cxscan
+cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS})"
+
+cxscan="${cxscan//'%'/'%25'}"
+cxscan="${cxscan//$'\n'/'%0A'}"
+cxscan="${cxscan//$'\r'/'%0D'}"
 echo "::set-output name=cxcli::$cxscan"
