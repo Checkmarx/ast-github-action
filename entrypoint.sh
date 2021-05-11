364dcdf62da5c3217ea815058bb94809d23d8aaa
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-05-11 13:08:05 +0100

    clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS})"
 
+echo "$cxscan"
+
 cxscan="${cxscan//'%'/'%25'}"
 cxscan="${cxscan//$'\n'/'%0A'}"
 cxscan="${cxscan//$'\r'/'%0D'}"
