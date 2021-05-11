3293339a6b22b638eed9b6a9c4f0b521eef1d0a0
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-05-11 11:50:24 +0100

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-OUTPUT=$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS})
-
-echo $OUTPUT
-
-echo "::set-output name=output::$OUTPUT"
+cxscan=`/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS}`
+echo $cxscan
+echo "::set-output name=cxcli::$cxscan"
