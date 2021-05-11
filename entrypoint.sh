7eea332634735091c836834f625018ee4b9c261f
Pedro Lopes <Pedro.Lopes@checkmarx.com>
2021-05-11 10:37:21 +0100

    test unit test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS}
+
+OUTPUT=$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --preset-name "${PRESET_NAME}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS})
+
+echo $OUTPUT
+
+echo "::set-output name=output::$OUTPUT"
