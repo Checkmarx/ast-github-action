fe97b679284d8854d273068902ac353a1abee7ef
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2021-05-13 08:21:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo "scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS}"
-
 cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS})"
 
 echo "$cxscan"
