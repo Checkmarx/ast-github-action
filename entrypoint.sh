204ca4e90f44c34631d9e3a8664786862aff3949
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2021-05-13 13:28:49 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,9 +3,9 @@
 #param= echo "--sast-preset-name \"Jay\""|sed 's@\\@@g'
 #echo $param
 
-param= "${ADDITIONAL_PARAMS}"|sed 's@\\@@g'
-echo $param
-cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" $param)"
+#param= "${ADDITIONAL_PARAMS}"|sed 's@\\@@g'
+#echo $param
+cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" '$ADDITIONAL_PARAMS')"
 
 echo "$cxscan"
 
