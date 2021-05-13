ff653aa84cb85b89e6d90fe8266dad8f5f0325c1
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-05-13 00:24:57 -0400

    Additional parameter

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-param= ${ADDITIONAL_PARAMS}|sed 's@\"@@g'
+param= ${ADDITIONAL_PARAMS}|sed 's@\\@@g'
 
 echo $param
 cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" $param)"
