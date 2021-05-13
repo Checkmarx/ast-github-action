bb6b7edcb3a050585f91c1f71874ecc0eee654ac
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2021-05-13 17:45:36 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-ADDITIONAL_PARAMS_ARR=("${ADDITIONAL_PARAMS}")
+echo $ADDITIONAL_PARAMS
+#ADDITIONAL_PARAMS_ARR=($ADDITIONAL_PARAMS)
 
-cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS_ARR[@]})"
+cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -d "." --filter "${FILTER}" --format json --agent "Github Action" ${ADDITIONAL_PARAMS[@]})"
 
 echo "$cxscan"
 
