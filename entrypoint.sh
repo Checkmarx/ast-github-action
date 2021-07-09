29a857ebf7cb157c1450ee38e111717dbadc2b95
Pedro Lopes <83576881+pedrompflopes@users.noreply.github.com>
2021-07-09 08:59:02 +0100

    Feature/branch name (#12)
    * add branch property

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 eval "arr=(${ADDITIONAL_PARAMS})"
 
 exec 5>&1
-cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -s "." --filter "${FILTER}" --format json --agent "Github Action" "${arr[@]}" | tee >(cat - >&5))"
+cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -s "." --filter "${FILTER}" --branch "${BRANCH}" --format json --agent "Github Action" "${arr[@]}" | tee >(cat - >&5))"
 
 cxscan="${cxscan//'%'/'%25'}"
 cxscan="${cxscan//$'\n'/'%0A'}"
