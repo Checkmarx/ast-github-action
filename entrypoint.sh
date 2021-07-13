bdc01b57217aafe41c97e8aca43249a4f1462a0c
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-07-13 10:34:07 -0400

    added the echo to print the value

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,5 @@ cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name
 cxscan="${cxscan//'%'/'%25'}"
 cxscan="${cxscan//$'\n'/'%0A'}"
 cxscan="${cxscan//$'\r'/'%0D'}"
-echo "::set-output name=cxcli::$cxscan"
+#echo "::set-output name=cxcli::$cxscan"
+echo $cxscan
