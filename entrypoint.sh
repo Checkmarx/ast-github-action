b4baf749de51da4dd431179ee1379719c32a7574
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-07-13 11:33:42 -0400

    changed the execution logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ eval "arr=(${ADDITIONAL_PARAMS})"
 
 exec 5>&1
 #cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -s "." --filter "${FILTER}" --branch "${BRANCH#refs/heads/}" --format json --agent "Github Action" "${arr[@]}" | tee >(cat - >&5))"
-/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -s "." --filter "${FILTER}" --branch "${BRANCH#refs/heads/}" --format json --agent "Github Action" "${arr[@]}" | tee >(cat - >&5)
-#cxscan="${cxscan//'%'/'%25'}"
-#cxscan="${cxscan//$'\n'/'%0A'}"
-#cxscan="${cxscan//$'\r'/'%0D'}"
+cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -s "." --filter "${FILTER}" --branch "${BRANCH#refs/heads/}" --format json --agent "Github Action" "${arr[@]}" | tee >(cat - >&5))" /etc/passwd > /dev/null 2>&1
+cxscan="${cxscan//'%'/'%25'}"
+cxscan="${cxscan//$'\n'/'%0A'}"
+cxscan="${cxscan//$'\r'/'%0D'}"
 #echo "::set-output name=cxcli::$cxscan"
 #value="Failed"
 #if [[ $cxscan =~ .*"$value"*. ]]; then
