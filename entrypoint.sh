b885c8ec100636586b7b5777c68e25ae8b8b6a49
Jay Nanduri <jay.nanduri@checkmarx.com>
2021-07-14 12:39:20 -0400

    changed the execution logic

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 eval "arr=(${ADDITIONAL_PARAMS})"
 
-exec 5>&1
+#exec 5>&1
 #cxscan="$(/app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -s "." --filter "${FILTER}" --branch "${BRANCH#refs/heads/}" --format json --agent "Github Action" "${arr[@]}" | tee >(cat - >&5))"
 /app/bin/cx scan create -v --scan-types "${SCAN_TYPES}" --project-name "${PROJECT_NAME}" -s "." --filter "${FILTER}" --branch "${BRANCH#refs/heads/}" --format json --agent "Github Action" "${arr[@]}"
 #cxscan="${cxscan//'%'/'%25'}"
