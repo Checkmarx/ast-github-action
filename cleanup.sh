a0463272dbb94e889823ea3c70f9b5661cb58a5c
AndreGCX <andre.goncalves@checkmarx.com>
2022-03-25 16:24:51 +0000

    regex to find scan-id from logs and scan cancel

--- cleanup.sh
+++ cleanup.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-if [ {{cancelled()}}]
-then
-  echo "Job cancelled, printing log file"
-  echo "$(cat ./output.log)"
-  
-/app/bin/cx scan cancel --scan-id
-exitCode=$?
+cancelId=(`grep -E '"(ID)":"((\\"|[^"])*)"' ./output.log | cut -d',' -f1 | cut -d':' -f2 | tr -d '"'`)
+echo "Cancelling scan with ID:" $cancelId
 
+/app/bin/cx scan cancel --scan-id "${cancelId}" --debug
+
+exitCode=$?
 echo "Program exits with code: " $exitCode
+
 if [ $exitCode -eq 0 ]
 then
-  echo "Scan completed"
+  echo "Scan cancelled successfully"
 
 else
-  echo "Scan Failed"
+  echo "Scan cancelation Failed"
   exit $exitCode
 fi
