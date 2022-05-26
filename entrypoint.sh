573531cf9819a51d4486c919ea37a59cabafda8a
Jay Nanduri <jay.nanduri@checkmarx.com>
2022-05-26 12:46:42 -0400

    update condition to check the branch value

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 
-BRANCH_VAL = "${BRANCH}"
-if [ -z "$BRANCH_VAL" ]
-  then
-    BRANCH_VAL = ${GITHUB_HEAD_REF}
-    echo "BRANCH_VAL is empty, using GITHUB_HEAD_REF: $BRANCH_VAL"
-  ELSE
-    BRANCH_VAL = ${BRANCH}
-    echo "BRANCH_VAL is not empty, using BRANCH: $BRANCH_VAL"
-fi
-
 eval "arr=(${ADDITIONAL_PARAMS})"
-/app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH_VAL}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i ./output.log
+/app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${GITHUB_HEAD_REF:-${BRANCH#refs/heads/}}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i ./output.log
 exitCode=${PIPESTATUS[0]}
 
 echo "Program exits with code: " $exitCode
