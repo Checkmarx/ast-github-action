a64186da760cbb8f9d3d88fd55ec331794f67f9d
Jay Nanduri <jay.nanduri@checkmarx.com>
2022-05-27 09:12:22 -0400

    change default branch value

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ echo "Branch name: " ${BRANCH}
 echo "Branch from github: " ${GITHUB_HEAD_REF}
 
 eval "arr=(${ADDITIONAL_PARAMS})"
-/app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/:-${GITHUB_HEAD_REF}}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i ./output.log
+/app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i ./output.log
 exitCode=${PIPESTATUS[0]}
 
 echo "Program exits with code: " $exitCode
