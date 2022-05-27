a1019c2a18fda5a1b2508e62b5d7b43ca823a0ea
Jay Nanduri <jay.nanduri@checkmarx.com>
2022-05-27 09:59:27 -0400

    cleanup echo logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-echo "Starting the application..."
-echo "Environment variables:" $@
-echo "Branch ref: " ${BRANCH#refs/heads/}
-echo "Branch name: " ${BRANCH}
-echo "Branch from github: " ${GITHUB_HEAD_REF}
-
 eval "arr=(${ADDITIONAL_PARAMS})"
 /app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i ./output.log
 exitCode=${PIPESTATUS[0]}
