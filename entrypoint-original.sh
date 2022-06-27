0cba9e2ee9e58d753f89a8c045b97ef185463c44
Jay Nanduri <jay.nanduri@checkmarx.com>
2022-06-26 22:14:43 -0400

    added step to decorate PR

---
+++ entrypoint-original.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+eval "arr=(${ADDITIONAL_PARAMS})"
+
+/app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i /tmp/output.log
+exitCode=${PIPESTATUS[0]}
+
+echo "Program exits with code: " $exitCode
+if [ $exitCode -eq 0 ]
+then
+  echo "Scan completed"
+else
+  echo "Scan Failed"
+  exit $exitCode
+fi
