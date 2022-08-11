fe744162cb2ccc0819547880aecab63f24e6a20e
Ludal <44783088+iamludal@users.noreply.github.com>
2022-08-11 15:59:44 +0200

    feat: add link to scan results in step summary (#1)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+output_file=./output.log
+
 eval "arr=(${ADDITIONAL_PARAMS})"
-/app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i ./output.log
+/app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i $output_file
 exitCode=${PIPESTATUS[0]}
 
 echo "Program exits with code: " $exitCode
@@ -12,3 +14,8 @@ else
   echo "Scan Failed"
   exit $exitCode
 fi
+
+scan_url=$(grep -oE 'https://(.+)' < $output_file)
+
+echo "# Checkmarx scan 🔒" >> $GITHUB_STEP_SUMMARY
+echo "🔗 [View results]($scan_url)" >> $GITHUB_STEP_SUMMARY
