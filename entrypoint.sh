05190d4ce04192fb7eec3eb69f28f49170f9280d
jay-nanduri <72423211+jay-nanduri@users.noreply.github.com>
2022-08-31 11:08:00 -0400

    pr decoration from gh action (#56)
    PR decoration from GH Actions

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,20 @@ eval "arr=(${ADDITIONAL_PARAMS})"
 /app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i $output_file
 exitCode=${PIPESTATUS[0]}
 
+scanId=(`grep -E '"(ID)":"((\\"|[^"])*)"' /tmp/output.log | cut -d',' -f1 | cut -d':' -f2 | tr -d '"'`)
+if [ -z "$scanId" ]
+then
+  echo "Scan not created. Terminating PR decoration job."
+else
+  if [ -z "${PR_NUMBER}" ]
+    then
+      echo "PR NUMBER not received to create decoration."
+    else
+      echo "Creating PR decoration for scan ID:" $scanId
+      /app/bin/cx utils pr github --scan-id "${scanId}" --namespace "${NAMESPACE}" --repo-name "${REPO_NAME}" --pr-number "${PR_NUMBER}" --token "${GITHUB_TOKEN}"
+  fi
+fi
+
 echo "Program exits with code: " $exitCode
 if [ $exitCode -eq 0 ]
 then
