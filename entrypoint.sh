9f841010f1b70e1b7b1084433fd5e26cdb1f203b
Andre Macedo <149069722+amacedoo@users.noreply.github.com>
2024-09-16 22:49:28 +0100

    fix attempt to permission denied for output.log

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,8 @@
 
 output_file=./output.log
 
+chown nonroot:nonroot /output.log
+
 eval "arr=(${ADDITIONAL_PARAMS})"
 /app/bin/cx scan create --project-name "${PROJECT_NAME}" -s "." --branch "${BRANCH#refs/heads/}" --scan-info-format json --agent "Github Action" "${arr[@]}" | tee -i $output_file
 exitCode=${PIPESTATUS[0]}
